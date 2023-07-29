fec82a5a11bf1aefa575d07aa673c4c39d3cf150
Kirari04 <lev@saminskij.ch>
2023-07-29 16:57:42 +0200

    removed token logs

--- layouts/panel.vue
+++ layouts/panel.vue
@@ -18,7 +18,6 @@ import { ServerConfig } from "composables/states";
 
 const token = useToken();
 const router = useRouter();
-console.log("token val", token.value);
 if (!token.value) {
     router.push("/login");
 }
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const token = useToken();
     const router = useRouter();
-    console.log("token val", token.value)
     if (!token.value) {
         router.push("/login");
     }
