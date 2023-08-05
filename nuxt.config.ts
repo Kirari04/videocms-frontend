faf443d20d37d8536ad5c706e3c99104f9d861fb
Kirari04 <lev@saminskij.ch>
2023-08-05 18:11:53 +0200

    added support for env config after build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,24 @@ export default defineNuxtConfig({
     modules: ["@nuxtjs/tailwindcss"],
     app: {
         pageTransition: { name: "page", mode: "out-in" },
+        head: {
+            script: [],
+        },
     },
     runtimeConfig: {
         public: {
-            apiUrl: "",
-            baseUrl: "",
-            dockerHubTag: "",
-            apiDocs: "",
+            apiUrl: process.env.NUXT_PUBLIC_API_URL
+                ? process.env.NUXT_PUBLIC_API_URL
+                : "",
+            baseUrl: process.env.NUXT_PUBLIC_BASE_URL
+                ? process.env.NUXT_PUBLIC_BASE_URL
+                : "",
+            dockerHubTag: process.env.NUXT_PUBLIC_DOCKER_HUB_TAG
+                ? process.env.NUXT_PUBLIC_DOCKER_HUB_TAG
+                : "",
+            apiDocs: process.env.NUXT_PUBLIC_API_DOCS
+                ? process.env.NUXT_PUBLIC_API_DOCS
+                : "",
         },
     },
     imports: {
