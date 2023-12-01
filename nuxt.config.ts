0b17919ca8560632f1b822f84a715f95693d0533
kirari04 <lev@saminskij.ch>
2023-12-01 23:32:12 +0100

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ["@nuxtjs/tailwindcss"],
+    ssr: false,
     app: {
         pageTransition: { name: "page", mode: "out-in" },
         head: {
