3eb12ed6e88530a947a52c31e4e268628370dba1
kirari04 <lev@saminskij.ch>
2024-01-08 20:39:35 +0100

    improoved encoding tab

--- pages/my/encodings.vue
+++ pages/my/encodings.vue
@@ -15,24 +15,36 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="task in data">
+                <tr v-if="datas.length === 0">
+                    <td colspan="3">
+                        No active encodings queued atm.
+                    </td>
+                </tr>
+                <tr v-for="task in datas">
                     <td>
                         {{ task.Name }}
                     </td>
                     <td>
-                        {{ task.Quality }}
+                        <span class=" badge badge-neutral">
+                            {{ task.Quality }}
+                        </span>
                     </td>
                     <td>
-                        <span :class="task.Progress == 0
-                            ? 'loading loading-spinner text-primary loading-lg'
-                            : 'radial-progress text-primary'
-                            " :style="`
-                                --value: ${task.Progress};
+                        <div class="flex items-center">
+                            <span :class="task.Progress === 0
+                                ? 'loading loading-spinner text-primary loading-lg'
+                                : 'radial-progress text-primary'
+                                " :style="`
+                                --value: ${task.Progress * 100};
                                 --size: 2.5rem;
                                 --thickness: 3px;
                             `">
-                            <span class="text-xs whitespace-nowrap">{{ `${task.Progress * 100}`.substring(0, 3) }} %</span>
-                        </span>
+                                <span class="text-xs whitespace-nowrap">
+                                    {{ `${task.Progress * 100}`.substring(0, 3) }} %
+                                </span>
+                            </span>
+                        </div>
+
                     </td>
                 </tr>
             </tbody>
@@ -48,24 +60,38 @@ definePageMeta({
 
 const conf = useRuntimeConfig();
 const token = useToken();
-const {
-    data,
-    refresh,
-} = useFetch<{
+interface Encoding {
     ID: number;
     Name: string;
     Quality: string;
     Progress: number;
-}[]>(`${conf.public.apiUrl}/encodings`, {
-    headers: {
-        Authorization: `Bearer ${token.value}`,
-    },
-});
+}
+const datas = ref<Encoding[]>([])
+const errors = ref<string | null>(null)
+
+async function load() {
+    const {
+        data,
+        error,
+    } = await useFetch<Encoding[]>(`${conf.public.apiUrl}/encodings`, {
+        headers: {
+            Authorization: `Bearer ${token.value}`,
+        },
+    });
+    if (error.value) {
+        errors.value = `${error.value.data}`
+        return
+    }
+    if (data.value) {
+        datas.value = data.value
+    }
+}
 
 let intv: NodeJS.Timeout | null = null;
 onMounted(() => {
+    load()
     intv = setInterval(() => {
-        refresh()
+        load()
     }, 5000)
 })
 onUnmounted(() => {
