f6dc6b250d53fbbea186e8e02f8ba23659d06e86
kingjulienboss <147748708+kingjulienboss@users.noreply.github.com>
2024-04-09 14:53:53 +0200

    added error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full h-screen gap-6">
+        <h2 class="text-4xl">{{ error?.statusCode }}</h2>
+        <button class="btn" @click="handleError">Back to Home</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+    error: Object as () => NuxtError
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
