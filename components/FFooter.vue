ee3fb4aff733117fe49606bc849b729a63ef2f15
kirari04 <lev@saminskij.ch>
2025-06-01 20:00:29 +0200

    added docs to footer

--- components/FFooter.vue
+++ components/FFooter.vue
@@ -3,6 +3,11 @@
         <nuxt-link :to="`/p${webPage.Path}`" v-for="webPage in webPages">
             {{ webPage.Title }}
         </nuxt-link>
+        <nuxt-link :to="'https://videocms-docs.vercel.app/'" target="_blank"
+            class="btn">
+            <img src="https://raw.githubusercontent.com/Kirari04/videocms/master/public/logo.png" class="w-6 h-6" />
+            Docs
+        </nuxt-link>
     </footer>
 </template>
 
@@ -10,4 +15,5 @@
 
 
 const { data: webPages } = useWebPage()
+const serverConfig = useServerConfig();
 </script>
